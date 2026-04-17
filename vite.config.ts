f9d5fef2019a7c84b4b124630ce2a661ee46d554
awu <awu@mitake.com.tw>
2026-04-17 16:14:21 +0800

    fix vite base

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   // 重要：把 'your-repo-name' 換成你 GitHub 儲存庫的名字
   // 記得前後都要有斜線，例如 '/ff14-marker-trainer/'
-  base: "/ff14-marker-trainer/",
+  base: "/awuuu0716.github.io/",
   server: {
     port: 3010,
   },
